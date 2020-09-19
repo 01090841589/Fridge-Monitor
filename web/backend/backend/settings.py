e0a36bceeaac6777d0529aaaaff5bb205cbb4912
01090841589 <chanchanhwan@naver.com>
2020-09-19 10:18:50 +0900

    보안이슈

--- web/backend/backend/settings.py
+++ web/backend/backend/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8o4#44uo-y3*bgx36!lfw*jru8edvtp3^zx3o6z-i6wih)ig^s'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
